dadc7ca9ca1e0a37ed6ef0032b3910d33e270a47
Aleksey <sjim2ez4u@gmail.com>
2018-11-23 01:22:19 +0400

    2nd lesson, 1st lecture

--- test_add_group.py
+++ test_add_group.py
@@ -9,22 +9,18 @@ class test_first(unittest.TestCase):
          self.wd = webdriver.Chrome()
          self.wd.implicitly_wait(30)
 
-     def test_first(self):
+     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
-        self.fill_group_form(wd, Group(name="test", header="test", footer="test1"))
+        self.create_group(wd, Group(name="test", header="test", footer="test1"))
         self.submit(wd)
         self.open_groups(wd)
         self.logout(wd)
 
      def test_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups(wd)
-        self.fill_group_form(wd, Group(name="", header="", footer=""))
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.submit(wd)
         self.open_groups(wd)
         self.logout(wd)
@@ -41,8 +37,11 @@ class test_first(unittest.TestCase):
          wd.find_element_by_name("submit").click()
 
 
-     def fill_group_form(self, wd, group):
+     def create_group(self, wd, group):
+         self.open_groups(wd)
+         #init_group_creation
          wd.find_element_by_name("new").click()
+         #fill_form
          wd.find_element_by_name("group_name").click()
          wd.find_element_by_name("group_name").clear()
          wd.find_element_by_name("group_name").send_keys(group.name)
@@ -52,9 +51,11 @@ class test_first(unittest.TestCase):
          wd.find_element_by_name("group_footer").click()
          wd.find_element_by_name("group_footer").clear()
          wd.find_element_by_name("group_footer").send_keys(group.footer)
+         self.return_to_group_page(wd)
 
 
      def login(self, wd, username, password):
+         self.open_home_page(wd)
          wd.find_element_by_name("user").click()
          wd.find_element_by_name("user").clear()
          wd.find_element_by_name("user").send_keys(username)
@@ -65,10 +66,11 @@ class test_first(unittest.TestCase):
          wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
 
+     def return_to_group_page(self, wd):
+         wd.find_element_by_link_text("group page").click()
+
      def open_home_page(self, wd):
          wd.get("http://localhost/addressbook/")
-
-
 
 
      def is_element_present(self, how, what):
