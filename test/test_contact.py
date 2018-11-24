ea0fe0f9b6cffa31e11ff3af253be8b6aabe0205
Aleksey <sjim2ez4u@gmail.com>
2018-11-24 07:22:19 +0400

    2nd week, lecture 3. Intermidiate commit.

--- test/test_contact.py
+++ test/test_contact.py
@@ -12,7 +12,7 @@ def app(request):
 
 
 def test_contacts(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.fill_form(Contact(firstname="test_firstname", middlename="test_middlename", lastname="test_lastname",
                                    nickname="test_nickname", title="test_title", company="test_company",
                                    home="test_home",
@@ -21,4 +21,4 @@ def test_contacts(app):
                                    homepage="test_homepage",
                                    notes="test_notes", phone2="test_phone2", address2="test_address2",
                                    bmonth="January", bday="1", byear="2000", amonth="February", aday="2", ayear="2002"))
-    app.logout()
+    app.session.logout()
