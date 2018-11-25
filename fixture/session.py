bdd36e30fddcd168ad84beab81c7f4fe3b6bc7c6
Aleksey <sjim2ez4u@gmail.com>
2018-11-26 02:27:12 +0400

    Week 3. Exercise 8.

--- fixture/session.py
+++ fixture/session.py
@@ -21,4 +21,27 @@ class SessionHelper:
         wd.find_element_by_link_text("Logout").click()
 
 
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
 
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_element_by_link_text("Logout")) > 0
+
+
+    def is_logged_in_as(self, usermame):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Address Book'])[1]/preceding::b[1]").text == "("+username+")"
+
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
