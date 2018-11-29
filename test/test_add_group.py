f0c0a2376feb7a56a735a315bdd86780db6baa34
Aleksey <sjim2ez4u@gmail.com>
2018-11-29 11:52:24 +0400

    Week 4. lecture 8. Загружаем информацию из тестируемого приложения в виде списков. all done

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,8 +5,11 @@ from model.group import Group
 def test_add_group(app):
     old_groups = app.group.get_group_list()
     app.group.create(Group(name="test", header="test", footer="test1"))
-    new_groups = app.group.get_group_list
+    new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
 
 def test_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
