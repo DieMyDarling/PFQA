22d72ae153aaf8de5a4ba0d4337623d9e2b8193c
Aleksey <sjim2ez4u@gmail.com>
2018-12-04 00:30:18 +0400

    Week 4. Excersice 11. add_contact is worked

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_contacts(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="test_firstname", lastname="test_lastname")
+    contact = Contact(firstname="test_firstname", lastname="test_lastname", bday="1", aday="2", bmonth="January", amonth="February")
     app.contact.create_user(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
