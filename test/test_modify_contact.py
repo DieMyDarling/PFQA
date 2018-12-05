e39ca10b7d67728e3a8b7aac115cc9f3df4e2fb7
Aleksey <sjim2ez4u@gmail.com>
2018-12-05 12:36:59 +0400

    Week 4. Excersice 11. Done

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,10 +6,18 @@ from model.contact import Contact
 def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.create_user(Contact(firstname="test_firstname1", bday="1", aday="2", amonth="January", bmonth="February"))
-    app.contact.modify_first(
-        Contact(title="test_title1", company="test_company1", address="test_address1", home="test_home1",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(title="test_title1", company="test_company1", address="test_address1", home="test_home1",
                 mobile="test_mobile1", work="test_work1", fax="test_fax1", email="test_email_1", email2="test_email2_2",
                 email3="test_email3_3", homepage="test_homepage1", firstname="test_firstname1",
                 middlename="test_middlename1", lastname="test_lastname1", nickname="test_nickname1",
                 notes="test_notes1", phone2="test_phone3", address2="test_address3", bday="7", bmonth="July",
-                byear="2007", aday="8", amonth="August", ayear="2008"))
+                byear="2007", aday="8", amonth="August", ayear="2008")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
