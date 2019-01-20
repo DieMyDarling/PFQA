8ed9607ecb1d1e8931286efce6a790e09dd62421
DieMyDarling <sjim2ez4u@gmail.com>
2019-01-20 18:27:13 +0400

    14

--- test/test_phones.py
+++ test/test_phones.py
@@ -4,25 +4,33 @@ import re
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert clear(contact_from_home_page.home) == clear(contact_from_edit_page.home)
-    assert clear(contact_from_home_page.work) == clear(contact_from_edit_page.work)
-    assert clear(contact_from_home_page.mobile) == clear(contact_from_edit_page.mobile)
-    assert clear(contact_from_home_page.phone2) == clear(contact_from_edit_page.phone2)
+    assert contact_from_home_page.all_emails == merge_email_like_on_homepage(contact_from_edit_page)
+    assert contact_from_home_page.all_phones == merge_phones_like_on_homepage(contact_from_edit_page)
 
-def test_contact(app):
-    contacts = list(app.contact.get_contact_list())
-    index = random.randrange(len(contacts))
-    contact_from_list = contacts[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_list.address == contact_from_edit_page.address
-    assert contact_from_list.firstname == contact_from_edit_page.firstname
-    assert contact_from_list.lastname == contact_from_edit_page.lastname
-    assert contact_from_list.all_emails or '\n\n' == "%s\n%s\n%s" % (
-        clear(contact_from_edit_page.email), clear(contact_from_edit_page.email2), clear(contact_from_edit_page.email3))
-    assert contact_from_list.all_phones or '\n\n\n' == "%s\n%s\n%s\n%s" % (
-        clear(contact_from_edit_page.home), clear(contact_from_edit_page.mobile),
-        clear(contact_from_edit_page.work), clear(contact_from_edit_page.phone2))
+
+# def test_contact(app):
+#     contacts = list(app.contact.get_contact_list())
+#     index = random.randrange(len(contacts))
+#     contact_from_list = contacts[index]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_list.address == contact_from_edit_page.address
+#     assert contact_from_list.firstname == contact_from_edit_page.firstname
+#     assert contact_from_list.lastname == contact_from_edit_page.lastname
+#     assert contact_from_list.all_emails == merge_email_like_on_homepage(contact_from_edit_page)
+#     assert contact_from_list.all_phones == merge_phones_like_on_homepage(contact_from_edit_page)
 
 
 def clear(s):
-    return re.sub("[-+()]", "", s)
+    return re.sub(r"[^\d\+]", "", s)
+
+def merge_phones_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "",
+                     map(lambda x: clear(x),
+                         filter(lambda x: x is not None,
+                                [contact.home, contact.mobile, contact.work, contact.phone2]))))
+
+
+def merge_email_like_on_homepage(contact):
+    return "\n".join(filter(lambda x: x != "",
+                     filter(lambda x: x is not None,
+                            [contact.email, contact.email2, contact.email3])))
